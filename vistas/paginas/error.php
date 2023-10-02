83142ac84affc40f75000827f09d58006e000d68
AdrianS100304 <jose13699631@gmail.com>
2023-10-01 19:53:31 -0600

    4C-jass-Wedding

---
+++ vistas/paginas/error.php
@@ -0,0 +1,185 @@
+<!--==== Header Section Here ======= -->
+<header class="header-section">
+    <div class="container">
+        <div class="header-wrapper">
+            <div class="logo-menu">
+                <a href="index.html" class="logo">
+                    <img src="html/assets/img/logo/logo1.png" alt="img">
+                </a>
+            </div>
+            <div class="header-bar d-lg-none">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul class="main-menu">
+                <li class="active">
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+                <li>
+                    <a href="#0">Service <i class="fas fa-chevron-down"></i></a>
+                    <ul class="sub-menu">
+                        <li class="subtwohober">
+                            <a href="service.html">
+                                <span>Service</span>
+                            </a>
+                        </li>
+                        <li class="subtwohober">
+                            <a href="service-single.html">
+                                <span>Service Single</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="story.html">Our story</a>
+                </li>
+                <li>
+                    <a href="#0">Pages <i class="fas fa-chevron-down"></i></a>
+                    <ul class="sub-menu">
+                        <li class="subtwohober">
+                            <a href="event.html">
+                                <span>Our event</span>
+                            </a>
+                        </li>
+                        <li class="subtwohober">
+                            <a href="gallery.html">
+                                <span>Gallery</span>
+                            </a>
+                        </li>
+                        <li class="subtwohober">
+                            <a href="error.html">
+                                <span>Error 404</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#0">Blog <i class="fas fa-chevron-down"></i></a>
+                    <ul class="sub-menu">
+                        <li class="subtwohober">
+                            <a href="blog.html">
+                                <span>Blog</span>
+                            </a>
+                        </li>
+                        <li class="subtwohober">
+                            <a href="blog-single.html">
+                                <span>Blog Single</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="contact.html">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+<!--==== Header Section End ======= -->
+
+<!--==== Scroll-Up Section Here ======= -->
+<div class="scroll-up">
+    <svg class="scroll-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+</div>
+<!--==== Scroll-Up Section End ======= -->
+
+<!--=========== Breadcumd Section Here ========= -->
+<section class="breadcumd__banner">
+    <div class="container">
+        <div class="breadcumd__wrapper center">
+            <h1 class="left__content">
+                error 404
+            </h1>
+            <ul class="right__content">
+                <li>
+                    <a href="index.html">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <i class="fa-solid fa-chevron-right"></i>
+                </li>
+                <li>
+                    404
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--=========== Breadcumd Section End ========= -->
+
+<!--=========== Error Section Here ========= -->
+<section class="error__section pt-120 pb-120">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
+                <div class="error__thumb">
+                    <img src="html/assets/img/404/404.png" alt="error__image">
+                </div>
+                <div class="error__content">
+                    <h3>
+                        Oops! Page Not Found!
+                    </h3>
+                    <p>
+                        We are really, but the page you requested is missing:( Perhaps searching again can help.
+                    </p>
+                    <a href="index.html" class="cmn--btn mt-3">
+                        back to home
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--=========== Error Section End ========= -->
+
+<!--=========== Footer Section Here ========= -->
+<div class="footer__section">
+    <div class="container">
+        <div class="border__area">
+            <div class="container">
+                <div class="footer__logo center">
+                    <a href="index.html">
+                        <img src="html/assets/img/logo/logo.png" alt="footer__logo">
+                    </a>
+                </div>
+                <div class="footer__bottom center">
+                    <p>© 2023 All Rights Reserved. Designed by <a href="#0" class="text-base-2">NextGenerationDev</a></p>
+                    <ul class="footer__icon">
+                        <li>
+                            <a href="#0"><i class="fa-brands fa-facebook-f"></i></a>
+                        </li>
+                        <li>
+                            <a href="#0"><i class="fa-brands fa-twitter"></i></a>
+                        </li>
+                        <li class="active">
+                            <a href="#0"><i class="fa-brands fa-linkedin-in"></i></a>
+                        </li>
+                        <li>
+                            <a href="#0"><i class="fa-brands fa-whatsapp"></i></a>
+                        </li>
+                        <li class="mr-none">
+                            <a href="#0"><i class="fa-brands fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                    <ul class="footer__menu">
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="about.html">about</a></li>
+                        <li><a href="service.html">service</a></li>
+                        <li><a href="story.html">Story</a></li>
+                        <li><a href="gallery.html">gallery</a></li>
+                        <li><a href="blog.html">blog</a></li>
+                        <li class="mr-none"><a href="contact.html">contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--=========== Footer Section End ========= -->
